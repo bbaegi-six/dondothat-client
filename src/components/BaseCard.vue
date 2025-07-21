02a22ad431554dd0bc96c122ff6d28424bc514ef
hyungEee <jihyung772@gmail.com>
2025-07-21 16:54:24 +0900

    feat: basecard 컴포넌트 추가 #2

---
+++ src/components/BaseCard.vue
@@ -0,0 +1,23 @@
+<!-- BaseCard.vue -->
+<template>
+  <div class="base-card">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseCard",
+};
+</script>
+
+<style scoped>
+.base-card {
+  background-color: #414141; /* 카드 배경색 */
+  border-radius: 16px; /* 모서리 둥글게 */
+  padding: 16px; /* 내부 여백 */
+  min-height: 78px; /* 높이 기본값 (필요시 조절 가능) */
+  display: flex;
+  align-items: center;
+}
+</style>
