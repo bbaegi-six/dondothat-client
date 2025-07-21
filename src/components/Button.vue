4a1bc9399af6b29eb92049bb214e47fe4d7acff6
hyungEee <jihyung772@gmail.com>
2025-07-21 16:42:40 +0900

    feat: button 컴포넌트 추가 #2

---
+++ src/components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+  <button
+    class="w-full h-14 py-3 rounded-lg text-white font-bold hover:bg-red-600 transition"
+    style="background-color: #ff5555"
+  >
+    {{ props.label }}
+  </button>
+</template>
+
+<script setup>
+const props = defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+});
+</script>
