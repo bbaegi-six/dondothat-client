7aaf37586f46e5ff4d35d0add5caeb15ad08934b
hyungEee <jihyung772@gmail.com>
2025-07-28 16:32:10 +0900

    fix: 전역 설정 수정 #22

--- src/App.vue
+++ src/App.vue
@@ -10,21 +10,27 @@
 </template>
 
 <script setup>
-import { computed } from 'vue';
-import { useRoute } from 'vue-router';
-import Nav from '@/components/layout/Nav.vue';
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+import Nav from "@/components/layout/Nav.vue";
 
 const route = useRoute();
 const shouldShowNav = computed(() => {
-  const noNavRoutes = ['/login', '/register', '/forgot-password'];
+  const noNavRoutes = ["/login", "/register", "/forgot-password"];
   return !noNavRoutes.includes(route.path);
 });
 </script>
 
 <style scoped>
+* {
+  background-color: #2f2f2f;
+}
 @media (max-width: 430px) {
   .w-\[390px\] {
     width: 100%;
   }
+}
+::-webkit-scrollbar {
+  display: none;
 }
 </style>
