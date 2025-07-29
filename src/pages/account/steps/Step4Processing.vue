7d58a01244c7e45257cea545fe252500c0cdcb90
westh07~ <westsunh@gmail.com>
2025-07-29 17:29:08 +0900

    fix: #21 detail한 디자인 수정

--- src/pages/account/steps/Step4Processing.vue
+++ src/pages/account/steps/Step4Processing.vue
@@ -29,7 +29,7 @@
     </div>
 
     <!-- Additional Info - Fixed at bottom -->
-    <div class="pb-5">
+    <div class="pb-10">
       <div class="flex justify-center">
         <div class="w-[328px] bg-[#414141] rounded-lg p-4">
           <div class="flex items-center gap-2 mb-2">
@@ -40,7 +40,7 @@
               viewBox="0 0 24 24"
             >
               <path
-                d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"
+                d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.94-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"
               />
             </svg>
             <span class="text-white text-sm font-medium">예상 완료 시간</span>
