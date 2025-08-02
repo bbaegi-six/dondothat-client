e98f9a25c89962112caf06211b1e7c3b6cb57b73
hoonssac <Hoonssac.dev@gmail.com>
2025-08-02 23:47:03 +0900

    Feat: #62 로그인 성공 시 홈으로 이동

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-import router from './router'
-import './assets/styles/global.css'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import router from './router';
+import './assets/styles/global.css';
 import { useAuthStore } from './stores/auth';
 
-const app = createApp(App)
-const pinia = createPinia()
+const app = createApp(App);
+const pinia = createPinia();
 
-app.use(pinia)
+app.use(pinia);
 
 // 앱 시작 시 인증 상태 확인
 const authStore = useAuthStore();
 await authStore.checkAuth(); // 비동기 작업이 완료될 때까지 기다립니다.
 
-app.use(router)
+app.use(router);
 
-app.mount('#app')
+app.mount('#app');
