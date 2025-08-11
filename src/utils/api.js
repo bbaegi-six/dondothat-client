e106b570cb1fa4f2b8bf5d06a3c5422b4a30faca
hoonssac <Hoonssac.dev@gmail.com>
2025-08-11 11:20:29 +0900

    Refactor: #93 로그인 페이지 로고 변경, api 요청 주소 수정

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080';
+  import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api';
 
 // API 기본 설정
 const api = axios.create({
