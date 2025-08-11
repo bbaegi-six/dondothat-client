bf6f3f23cd546da70f5474231d196edaa1891ed3
Hyunji-JANG <78188222+Hyunji-JANG@users.noreply.github.com>
2025-08-11 17:38:33 +0900

    Fix: #89 채팅 입력과 하단바 사이에 여백 제거

--- src/components/chat/ChatInput.vue
+++ src/components/chat/ChatInput.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="fixed bottom-20 left-1/2 transform -translate-x-1/2 max-w-[390px] w-full bg-default px-6 z-50"
+    class="fixed left-1/2 transform -translate-x-1/2 max-w-[390px] w-full bg-default px-6 z-50 bottom-[75px]"
   >
     <div class="flex gap-2 items-center">
       <div class="flex-1 relative">
